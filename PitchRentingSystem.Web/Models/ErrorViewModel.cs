82a9e2e6ca5577919385ca632d788ad411e4b6dc
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-06-03 09:44:30 +0300

    Initial commit

---
+++ PitchRentingSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PitchRentingSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
