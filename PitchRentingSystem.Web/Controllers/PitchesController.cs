f73e4423779098c6c2570134a9a62e914442d6d6
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-06-03 18:42:22 +0300

    model for all pitches info

---
+++ PitchRentingSystem.Web/Controllers/PitchesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PitchRentingSystem.Web.Models.Pitches;
+
+namespace PitchRentingSystem.Web.Controllers
+{
+    public class PitchesController : Controller
+    {
+        public IActionResult All()
+        {
+            var allPitches = new AllPitchesViewModel()
+            {
+                Pitches = Common.GetPitches()
+            };
+            return View(allPitches);
+        }
+
+        public IActionResult Details()
+        {
+            var pitches  = Common.GetPitches().FirstOrDefault();
+            return View(pitches);
+        }
+    }
+}
---
+++ PitchRentingSystem.Web/Models/Pitches/AllPitchesViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PitchRentingSystem.Web.Models.Pitches
+{
+    public class AllPitchesViewModel
+    {
+        public IEnumerable<PitchesDetailsViewModel> Pitches { get; set; }
+            = new List<PitchesDetailsViewModel>();
+    }
+}
