4fcac613d27a1622d093ab7df1736e2fec2efe13
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-06-04 10:22:59 +0300

    Created Entity models

---
+++ PitchRentingSystem.Web/Data/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace PitchRentingSystem.Web.Data.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+    using static PitchRentingSystem.Web.Data.Constants.EntityConstants.CategoryConstants;
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(NameMaxLength), Required]
+        public string Name { get; set; } = null!;
+
+        public ICollection<Pitch> Pitches { get; set; } = new HashSet<Pitch>();
+    }
+}
