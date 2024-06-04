809063af508d05dbf87db40567da4049a1bf9343
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-06-04 10:22:45 +0300

    Created constant for the models

---
+++ PitchRentingSystem.Web/Data/Constants/EntityConstants.cs
@@ -0,0 +1,24 @@
+﻿namespace PitchRentingSystem.Web.Data.Constants
+{
+    public class EntityConstants
+    {
+        public const string DefaultPassword = "123456";
+
+        public class PitchConstants
+        {
+            public const int TitleMaxLength = 50;
+            public const int AddressMaxLength = 150;
+            public const int DescriptionMaxLenth = 500;
+        }
+
+        public class CategoryConstants
+        {
+            public const int NameMaxLength = 50;
+        }
+
+        public class AgentConstants
+        {
+            public const int PhoneNumberMaxLength = 15;
+        }
+    }
+}
